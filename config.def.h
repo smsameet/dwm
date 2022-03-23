992d2df3a571d8fd0a31c8f14535794027309411
sameet <sameetmoosavi@yahoo.com>
2022-03-23 22:27:31 +0430

    updating mod key ( super key )

--- config.def.h
+++ config.def.h
@@ -49,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
