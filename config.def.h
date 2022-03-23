09cdcc806bc559df5a3497c5048622517d2dfcb1
sameet <sameetmoosavi@yahoo.com>
2022-03-23 22:10:25 +0430

    adding alt key

--- config.def.h
+++ config.def.h
@@ -49,6 +49,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
