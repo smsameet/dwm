49ce35ddffb5a42d8298ce80e624986baa67a835
sameet <sameetmoosavi@yahoo.com>
2022-05-19 10:31:49 +0430

    remove anonymous shortcuts

--- config.def.h
+++ config.def.h
@@ -71,10 +71,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
 
 /* commands */
-static const char *filemanager[]         = { "nautilus", NULL };
-static const char *browser[]             = { "firefox", NULL };
-static const char *emails[]              = { "thunderbird", NULL };
-
 static char dmenumon[2]                  = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]            = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]             = { "kitty", NULL };
